f1b35e2006880302dfc620438c12c0d7029579ce
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2025-01-28 14:25:54 +0630

    user logout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,9 +1,26 @@
+import { useAuth } from "@/hooks"
+import { Navigate, Outlet, useLocation } from "react-router-dom"
+import ProfileBox from "./common/ProfileBox"
+import DesktopSidebar from "@/components/sidebar/DesktopSidebar"
+
 const DefaultLayout = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+	const { isAuthenticated } = useAuth()
+	const location = useLocation()
+
+	return !isAuthenticated ? (
+		<Navigate to={"/auth/login"} state={{ from: location }} replace />
+	) : (
+		<div className="h-svh flex overflow-hidden">
+			<DesktopSidebar />
+			<main className="w-full min-h-full overflow-y-auto">
+				<nav className="flex justify-end p-3">
+					<ProfileBox />
+				</nav>
+
+				<Outlet />
+			</main>
+		</div>
+	)
 }
 
 export default DefaultLayout
