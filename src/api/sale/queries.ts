6de836ee10f71ab947802f16a4337627db0b7479
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2025-01-29 15:53:23 +0630

    sale

--- src/api/sale/queries.ts
+++ src/api/sale/queries.ts
@@ -1,56 +1,29 @@
-// import { useMutation, useQuery, useQueryClient, type UseMutationOptions, type UseQueryOptions } from '@tanstack/vue-query'
-import addSaleServices from "./services";
-import getSaleServices from "./services";
-import getSaleSummaryServices from "./services";
+import { SaleType } from "@/shared/types";
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
+import axios from "axios";
 
-import type { AddSaleType, GetAllSaleType, GetSaleSummary } from "./types";
-import type { APIResponse } from "../config";
-import {
-  useMutation,
-  UseMutationOptions,
-  useQuery,
-  useQueryClient,
-  UseQueryOptions,
-} from "@tanstack/vue-query";
+export const getallsale = {
+  useQuery: (
+    opt?: Partial<UseQueryOptions<unknown, Error, Array<SaleType>>>,
+    onError?: () => void
+  ) => {
+    return useQuery({
+      queryKey: ["getAllSales"],
+      queryFn: async () => {
+        try {
+          const response = await axios.get("Sale/GetSaleReport");
+          const { data, status } = response.data;
 
-export const addSale = {
-  useMutation: (opt?: UseMutationOptions<any, Error, AddSaleType, any>) => {
-    const queryClient = useQueryClient();
-    return useMutation({
-      mutationKey: ["addSale"],
-      mutationFn: (payload: AddSaleType) => addSaleServices.createSale(payload),
-      onSuccess: () => {
-        queryClient.invalidateQueries({ queryKey: ["createSale"] });
+          if (status !== 0) {
+            onError?.();
+            return new Error("Error While Fetching");
+          }
+          return data;
+        } catch {
+          throw new Error("Error While Fetching");
+        }
       },
       ...opt,
     });
   },
-};
-
-export const fetchSales = {
-  useQuery: (opt?: UseQueryOptions<GetAllSaleType[], Error>) =>
-    useQuery<GetAllSaleType[], Error>({
-      queryKey: ["getallproduct"],
-      queryFn: async () => {
-        const response: APIResponse<GetAllSaleType[]> =
-          await getSaleServices.getAllSale();
-
-        return response.data;
-      },
-      ...opt,
-    }),
-};
-
-export const fetchSaleSummary = {
-  useQuery: (opt?: UseQueryOptions<GetSaleSummary, Error>) =>
-    useQuery<GetSaleSummary, Error>({
-      queryKey: ["getsalesummary"],
-      queryFn: async () => {
-        const response: APIResponse<GetSaleSummary> =
-          await getSaleSummaryServices.getSaleSummary();
-
-        return response.data;
-      },
-      ...opt,
-    }),
 };
--- src/api/sale/services.ts
+++ src/api/sale/services.ts
@@ -1,22 +1,31 @@
-import axiosInstance, { APIResponse } from "../config"
-import { AddSaleType, GetAllSaleType, GetSaleSummary } from "./types"
+// import axiosInstance, { APIResponse } from "../config";
+// import { AddSaleType, GetAllSaleType, GetSaleSummary } from "./types";
 
-const baseUrl = "/Sale"
-const createSale = async (sale: AddSaleType): Promise<APIResponse<AddSaleType>> => {
-    const response = await axiosInstance.post<APIResponse<AddSaleType>>(`${baseUrl}/AddSale`, sale)
+// const baseUrl = "/Sale";
+// const createSale = async (
+//   sale: AddSaleType
+// ): Promise<APIResponse<AddSaleType>> => {
+//   const response = await axiosInstance.post<APIResponse<AddSaleType>>(
+//     `${baseUrl}/AddSale`,
+//     sale
+//   );
 
-    return response.data
-}
+//   return response.data;
+// };
 
-const getAllSale = async (): Promise<APIResponse<GetAllSaleType[]>> => {
-    const response = await axiosInstance.get<APIResponse<GetAllSaleType[]>>(`${baseUrl}/GetSaleReport`)
+// const getAllSale = async (): Promise<APIResponse<GetAllSaleType[]>> => {
+//   const response = await axiosInstance.get<APIResponse<GetAllSaleType[]>>(
+//     `${baseUrl}/GetSaleReport`
+//   );
 
-    return response.data
-}
+//   return response.data;
+// };
 
-const getSaleSummary = async () : Promise<APIResponse<GetSaleSummary>> => {
-    const response = await axiosInstance.get<APIResponse<GetSaleSummary>>(`${baseUrl}/GetSaleSummary`)
+// const getSaleSummary = async (): Promise<APIResponse<GetSaleSummary>> => {
+//   const response = await axiosInstance.get<APIResponse<GetSaleSummary>>(
+//     `${baseUrl}/GetSaleSummary`
+//   );
 
-    return response.data
-}
-export default {createSale, getAllSale, getSaleSummary}
+//   return response.data;
+// };
+// export default { createSale, getAllSale, getSaleSummary };
