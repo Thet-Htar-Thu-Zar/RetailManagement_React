0e913f3f288e1983834aa343a474cd8d7f56beac
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2025-01-29 15:53:05 +0630

    add product services

--- src/api/product/services.ts
+++ src/api/product/services.ts
@@ -1,45 +1,48 @@
-import axiosInstance, { type APIResponse } from "../config";
-import {
-  DeleteProductType,
-  GetAllProductType,
-  UpdateProductInputType,
-} from "./types";
-const baseUrL = "/Product";
+// import { APIResponse } from "@/shared/types";
+// import {
+//   DeleteProductType,
+//   GetAllProductType,
+//   UpdateProductInputType,
+// } from "./types";
+// import axios from "axios";
 
-const getAllProduct = async (): Promise<APIResponse<GetAllProductType[]>> => {
-  const response = await axiosInstance.get<APIResponse<GetAllProductType[]>>(
-    `${baseUrL}/GetAllProduct`
-  );
+// const baseUrL = "Product";
 
-  return response.data;
-};
+// const getAllProduct = async (): Promise<APIResponse<GetAllProductType[]>> => {
+//   const response = await axios.get<APIResponse<GetAllProductType[]>>(
+//     `${baseUrL}/GetAllProduct`
+//   );
 
-const addProduct = async (
-  data: any
-): Promise<APIResponse<GetAllProductType>> => {
-  const response = await axiosInstance.post<APIResponse<GetAllProductType>>(
-    `${baseUrL}/AddProduct`,
-    data
-  );
+//   return response.data;
+// };
 
-  return response.data;
-};
+// const addProduct = async (
+//   data: any
+// ): Promise<APIResponse<GetAllProductType>> => {
+//   const response = await axios.post<APIResponse<GetAllProductType>>(
+//     `${baseUrL}/AddProduct`,
+//     data
+//   );
 
-const updateProduct = async (
-  data: any
-): Promise<APIResponse<UpdateProductInputType>> => {
-  const response = await axiosInstance.post<
-    APIResponse<UpdateProductInputType>
-  >(`${baseUrL}/UpdateProduct`, data);
+//   return response.data;
+// };
 
-  return response.data;
-};
+// const updateProduct = async (
+//   data: any
+// ): Promise<APIResponse<UpdateProductInputType>> => {
+//   const response = await axios.post<APIResponse<UpdateProductInputType>>(
+//     `${baseUrL}/UpdateProduct`,
+//     data
+//   );
 
-const deleteProduct = async (payload: DeleteProductType): Promise<any> => {
-  const response = await axiosInstance.delete(`${baseUrL}/DeleteProduct`, {
-    data: payload,
-  });
-  return response.data;
-};
+//   return response.data;
+// };
 
-export default { getAllProduct, addProduct, updateProduct, deleteProduct };
+// const deleteProduct = async (payload: DeleteProductType): Promise<any> => {
+//   const response = await axios.delete(`${baseUrL}/DeleteProduct`, {
+//     data: payload,
+//   });
+//   return response.data;
+// };
+
+// export default { getAllProduct, addProduct, updateProduct, deleteProduct };
